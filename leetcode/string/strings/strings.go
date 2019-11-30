815c297952c708e0f74d6b719e99e9db1c327ec6
zhufayuan <littlelittlezhu@users.noreply.github.com>
2019-11-30 10:18:44 +0800

    Add files via upload

--- leetcode/string/strings/strings.go
+++ leetcode/string/strings/strings.go
@@ -12,15 +12,54 @@ func reverseStr(source string) (result string) {
 		return ""
 	}
 	length := len(source)
-	var tempStr string
+	
 	for i := length - 1; i >= 0; i-- {
 		// 从字符串尾部开始遍历，分离出每一个单词（翻转）
 		if string(source[i]) != " " {
-			tempStr = tempStr + string(source[i])
+			tempStr := tempStr + string(source[i])
+			continue	
 		}
-		
-	}
+		if string(source[i]) == " " && len(tempStr) != 0 {
+			result = append(result, tempStr)
+			if result != "" {
+				result = append(result, " ")
+			}
+			
+		}
 
+		tempStr = ""
+	}
+	return result
+}
+
+func palindrome(source string, left, right int) (result string) {
+	if left >= 0 && right < len(source) && source[left] == source[right] {
+		left--
+		right++
+	}
+	return source[left:right + 1]
+}
+
+func longestPalindrSub(source string) (result string) {
+	if source == "" {
+		return ""
+	}
+	var tempSub string
+	for i, _ := range source {
+		tempSub = palindrome(source, i, i)
+		if len(tempSub) > len(result) {
+			result = tempSub
+		}
+		tempSub = palindrome(source, i, i + 1)
+		if len(tempSub) > len(result) {
+			result = tempSub
+		}
+	}
+	return result
+}
+
+func spaceRep(source string) (result string) {
+	
 }
 
 func main() {
